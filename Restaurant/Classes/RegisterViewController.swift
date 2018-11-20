7f8d503df60aa1b49f33cc73f43ca1711f1b9761
Yafet22 <yafet.trakan@gmail.com>
2018-11-20 18:12:06 +0700

    Merge Yudho + Yafet
    Semoga bener

---
+++ Restaurant/Classes/RegisterViewController.swift
@@ -0,0 +1,30 @@
+//
+//  RegisterViewController.swift
+//  Restaurant
+//
+//  Created by Yafets Mac on 16/11/18.
+//  Copyright © 2018 AppsFoundation. All rights reserved.
+//
+
+import UIKit
+
+class RegisterViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
