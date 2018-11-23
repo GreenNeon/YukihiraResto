ac0612e9cb69943bb808c84c3a88181efb217218
Neon <lab_pk_29@lab-pk-29s-Mac-mini.local>
2018-11-23 13:34:04 +0700

    Register

--- Restaurant/Classes/RegisterViewController.swift
+++ Restaurant/Classes/RegisterViewController.swift
@@ -7,15 +7,44 @@
 //
 
 import UIKit
+import Alamofire
 
 class RegisterViewController: UIViewController {
 
+    @IBOutlet weak var Email: UITextField!
+    @IBOutlet weak var Password: UITextField!
+    @IBOutlet weak var Nama: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
     }
     
+    func alertwoy(msg: String){
+        var alert  = UIAlertController(title: "ALERT", message: msg, preferredStyle: UIAlertControllerStyle.alert)
+        let ok = UIAlertAction(title: "OK", style: UIAlertActionStyle.default, handler: nil)
+        alert.addAction(ok)
+        present(alert, animated: true, completion: nil)
+    }
+    @IBAction func DoRegister(_ sender: Any) {
+        let parameters: Parameters = [
+            "name": Nama.text,
+            "phone": 01991,
+            "email": Email.text,
+            "password": Password.text,
+            "checkpassword": Password.text
+            ]
+        Alamofire.request("http://platform.yafetrakan.com/api/users", method: .post, parameters: parameters).responseJSON { response in
+            debugPrint(response)
+            
+            if let json = response.result.value {
+                if json != nil {
+                    self.alertwoy(msg: "Berhasil Login!!");
+                }
+            }
+        }
+    }
 
     /*
     // MARK: - Navigation
